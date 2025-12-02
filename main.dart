50463fa7149233d785e1c8347fc0424ab140cad4
risanafebina-hue <risanafebina@gmail.com>
2025-12-02 14:28:55 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:amazoneproject/splash.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+      ),
+      home: Splash(),
+    );
+  }
+}
